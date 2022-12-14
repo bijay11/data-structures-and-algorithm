56cf57d21730e93892643b0d5b2a458ad7c27d75
Bijay Tamang <tmg.bijay11@gmail.com>
2022-12-14 10:12:58 -0500

    is palindrome

---
+++ Recursion/isPalindrome/index.js
@@ -0,0 +1,26 @@
+// Write a recursive function called isPalindrome which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str) {
+  let reverseStr = str;
+  // add whatever parameters you deem necessary - good luck!
+
+  function helper(reverseStr) {
+    if (reverseStr <= 1) return reverseStr;
+
+    return helper(reverseStr.slice(1)) + reverseStr[0];
+  }
+
+  return helper(reverseStr) === str;
+}
+
+console.log(isPalindrome("awesome"));
+console.log(isPalindrome("foobar"));
+console.log(isPalindrome("tacocat"));
+console.log(isPalindrome("amanaplanacanalpanama"));
+console.log(isPalindrome("amanaplanacanalpandemonium"));
