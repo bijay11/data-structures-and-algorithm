788c97698df500f2834186e75c846dc305b73e4d
Bijay Tamang <tmg.bijay11@gmail.com>
2022-12-20 10:10:49 -0500

    implement binary search tree

---
+++ Data Structures/Binary Serach Tree/index.js
@@ -0,0 +1,46 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    if (value == null) return undefined;
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    }
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return "Value already eixists !!";
+
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
+        }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
+      }
+    }
+  }
+}
+
+const newBTS = new BinarySearchTree();
+newBTS.insert(20);
+newBTS.insert(14);
+newBTS.insert(30);
+newBTS.insert(15);
+console.log("test newBTS", newBTS);
